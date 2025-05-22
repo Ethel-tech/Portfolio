bc9c11bdcd42a5552eb501c56898b653db2ddf92
Ethel <ethelsimjaja@gmail.com>
2025-05-22 15:26:23 +0100

    added more projects

--- src/components/ReusableComponents/ProjectImages.js
+++ src/components/ReusableComponents/ProjectImages.js
@@ -1,42 +1,82 @@
 export const images = [
-  {
-    id: 0,
-    imgLink: "https://i.postimg.cc/Gm2y2Kpb/Screenshot-305.png",
-    altText: "Counter App",
-    title: "Counter App",
-    description:
-      "Two simple counter apps, one done with useReducer and the other done with Custom Hooks. The counter app changes background color on click of any buttons in the counter app, and a page to test errors.",
-    liveSite: "https://csb-jyq7n9.netlify.app/",
-    githubLink: "https://github.com/Ethel-tech/Counter-App",
-  },
-  {
-    id: 1,
-    imgLink: "https://i.postimg.cc/kMQwS9Wf/Screenshot-312.png",
-    altText: "Chatter App",
-    title: "Chatter App",
-    description:
-      "Chatter is a clone of a social publishing platforms, with features like login, sign up, create posts, create tags, etc.",
-    liveSite: "https://stunning-cat-41fa5e.netlify.app/",
-    githubLink: "https://github.com/Ethel-tech/my-chatter-app",
-  },
-  {
-    id: 2,
-    imgLink: "https://i.postimg.cc/CKnnCKDt/Screenshot-303.png",
-    altText: "Fetching Github Repo",
-    title: "Fetching Github Repo",
-    description:
-      "I created a mini portfolio where I fetched my github API and implemented it through routes and nested routes. This project was done with vue.js.",
-    liveSite: "https://u9p4pg.csb.app/",
-    githubLink: "https://github.com/Ethel-tech/csb-gnfor7",
-  },
-  {
-    id: 3,
-    imgLink: "https://i.postimg.cc/Yq8FMZMX/Screenshot-310.png",
-    altText: "Calculator",
-    title: "Calculator",
-    description:
-      "A simple calculator app built in React. Checkout the project in codesandbox",
-    liveSite: "https://u9p4pg.csb.app/",
-    githubLink: "https://github.com/Ethel-tech/Calculator",
-  },
+	{
+		id: 0,
+		imgLink: "https://i.postimg.cc/9fnNDTTj/Screenshot-2025-05-22-112947.png",
+		altText: "Client's Portfolio",
+		title: "Client's Portfolio",
+		description:
+			"Built a portfolio website for a client. Client provided the design, he wanted it modeled after an exsiting website.",
+		liveSite: "https://chidilevi-portfolio.netlify.app/",
+		githubLink: "https://github.com/Ethel-tech/chidi-portfolio",
+	},
+	{
+		id: 1,
+		imgLink: "https://i.postimg.cc/9QckKcVs/Screenshot-2025-05-22-112909.png",
+		altText: "Applora",
+		title: "Applora",
+		description:
+			"A solution aimed at streamlining operations for freelancers. It offers a platform for freelancers to manage their projects, clients, and finances in one place. The app is designed to be user-friendly and efficient, making it easier for freelancers to focus on their work.",
+		liveSite: "https://csb-jyq7n9.netlify.app/",
+		githubLink: "https://github.com/Ethel-tech/Counter-App",
+	},
+	{
+		id: 2,
+		imgLink: "https://i.postimg.cc/k54pMw2c/Screenshot-2025-05-22-113150.png",
+		altText: "Hair by Hormii",
+		title: "Hair by Hormii",
+		description:
+			"A website for a hair stylist. The website is designed to showcase the stylist's work, provide information about services, and allow clients to book appointments online.",
+		liveSite: "https://hairbyhormii.netlify.app/",
+		githubLink: "https://github.com/Ethel-tech/HairbyHormii",
+	},
+	{
+		id: 3,
+		imgLink: "https://i.postimg.cc/SsY1Cs7Y/Screenshot-2025-05-22-113341.png",
+		altText: "Visa Paper",
+		title: "Visa Paper",
+		description:
+			"A clone website of iVisa website.",
+		liveSite: "https://visa-paper.netlify.app/",
+		githubLink: "https://github.com/Ethel-tech/visa-paper",
+	},
+	{
+		id: 4,
+		imgLink: "https://i.postimg.cc/Gm2y2Kpb/Screenshot-305.png",
+		altText: "Counter App",
+		title: "Counter App",
+		description:
+			"Two simple counter apps, one done with useReducer and the other done with Custom Hooks. The counter app changes background color on click of any buttons in the counter app, and a page to test errors.",
+		liveSite: "https://csb-jyq7n9.netlify.app/",
+		githubLink: "https://github.com/Ethel-tech/Counter-App",
+	},
+	{
+		id: 5,
+		imgLink: "https://i.postimg.cc/kMQwS9Wf/Screenshot-312.png",
+		altText: "Chatter App",
+		title: "Chatter App",
+		description:
+			"Chatter is a clone of a social publishing platforms, with features like login, sign up, create posts, create tags, etc.",
+		liveSite: "https://stunning-cat-41fa5e.netlify.app/",
+		githubLink: "https://github.com/Ethel-tech/my-chatter-app",
+	},
+	{
+		id: 6,
+		imgLink: "https://i.postimg.cc/CKnnCKDt/Screenshot-303.png",
+		altText: "Fetching Github Repo",
+		title: "Fetching Github Repo",
+		description:
+			"I created a mini portfolio where I fetched my github API and implemented it through routes and nested routes. This project was done with vue.js.",
+		liveSite: "https://u9p4pg.csb.app/",
+		githubLink: "https://github.com/Ethel-tech/csb-gnfor7",
+	},
+	{
+		id: 7,
+		imgLink: "https://i.postimg.cc/Yq8FMZMX/Screenshot-310.png",
+		altText: "Calculator",
+		title: "Calculator",
+		description:
+			"A simple calculator app built in React. Checkout the project in codesandbox",
+		liveSite: "https://u9p4pg.csb.app/",
+		githubLink: "https://github.com/Ethel-tech/Calculator",
+	},
 ];
